0956712ea276d0db6cb3d7cc59431a98e9bb9417
sangwoo <138969571+rejeton1@users.noreply.github.com>
2023-08-15 18:13:17 +0900

    Add files via upload

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,5 @@
+library(stats)
+library(base)
 library(dplyr)
 library(stringr)
 
@@ -14,68 +16,59 @@ label <- suppressWarnings(
   c(as.numeric(y_test[[1]]), as.numeric(y_train[[1]])))
 data[1:10297,1] <- label
 
-# for(i in 1:2946){
-#     x_test[i,1] <- str_trim(x_test[i,1])
-#     splitted <- strsplit(x_test[i,1], " ")
-#     good <- splitted[[1]] != ""
-#     value <- splitted[[1]][good]
-#     data[i, 2:562] <- value
-# }
-# 
-# for(i in 1:7351){
-#   x_train[i,1] <- str_trim(x_train[i,1])
-#   splitted <- strsplit(x_train[i,1], " ")
-#   good <- splitted[[1]] != ""
-#   value <- splitted[[1]][good]
-#   data[i, 2:562] <- value
-# }
+for(i in 1:3){
+    x_test[i,1] <- str_trim(x_test[i,1])
+    splitted <- strsplit(x_test[i,1], " ")
+    value <- splitted[[1]][splitted[[1]] != ""]
+    data[i, 2:562] <- value
+}
 
-data[1, 2:10]
+for(i in 1:5){
+  x_train[i,1] <- str_trim(x_train[i,1])
+  splitted <- strsplit(x_train[i,1], " ")
+  value <- splitted[[1]][splitted[[1]] != ""]
+  data[i+3, 2:562] <- value
+}
 
 
 
+# descriptive activity name
+for(i in 1:10297){
+  activity_names <- c("WALKING", "WALKING_UPSTAIRS",
+                      "WALKING_DOWNSTAIRS", "SITTING",
+                      "STANDING", "LAYING")
+  data[i,1] <- activity_names[suppressWarnings(as.numeric(data[i,1]))]
+}
 
 
 
+#descriptive variable names
+variable_names <- read.csv("UCI HAR Dataset/features.txt", 
+                           header = FALSE, sep="|")
+variables <- c()
+for(i in 1:561){
+  variables[i] <- strsplit(variable_names[[1]][i], " ")[[1]][2]
+}
+
+names(data)[1] <- "activity"
+names(data)[2:562] <- variables
+
+#extracting only mean and std
+meanorstd <- grepl("mean", names(data)) | grepl("std", names(data))
+meanorstd[1] <- TRUE
+
+mean_std_data <- data[, meanorstd]
 
 
+#adding subject column
+subjects <- suppressWarnings(
+  c(as.numeric(subject_test[[1]]), as.numeric(subject_train[[1]])))
+mean_std_data[1:10297,81] <- subjects
+names(mean_std_data)[81] <- "subject"
 
-#data binding
-# test <- cbind(x_test, y_test)
-# train <- cbind(x_train, y_train)
-# names <- c("value", "activity")
-# names(test) <- names
-# names(train) <- names
+mean_std_data[1,]
 
-#data merging
-# merged_data <- rbind(test, train)
+#grouping by activity and subject
+group_data <- group_by(mean_std_data, activity, subject)
 
-# #extracting only mean and std
-# mean_std <- data.frame(activity=merged_data[,2], mean=c(0),
-#                        std=c(0))
-
-# for(i in 1:10297){
-#   merged_data[i,3] <- str_trim(merged_data[i,1])
-#   split_value <- strsplit(merged_data[i,1], " ")
-#   good <- split_value[[1]] != ""
-#   split_good <- split_value[[1]][good]
-#   split_good <- as.numeric(split_good)
-#   
-#   mean <- mean(split_good)
-#   std <- sd(split_good)
-#   mean_std[i,2] <- mean
-#   mean_std[i,3] <- std
-# }
-
-
-#descriptive activity name
-# for(i in 1:10297){
-#   activity_names <- c("WALKING", "WALKING_UPSTAIRS",
-#                       "WALKING_DOWNSTAIRS", "SITTING",
-#                       "STANDING", "LAYING")
-#   mean_std[i,1] <- activity_names[suppressWarnings(as.numeric(mean_std[i,1]))]
-# }
-
-# 
-# head(mean_std)
-
+data_by_act_sub <- summarise(group_data, )
